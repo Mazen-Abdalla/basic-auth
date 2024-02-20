54c743180f54e77e4fdd4049f1467aae6feeca44
Mazen-Abdalla <mazen.abdalla1@outlook.com>
2024-02-20 17:20:29 +0200

    api link update

--- app/src/api/axios.ts
+++ app/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_URL || "https://localhost:3500/api";
+const BASE_URL = import.meta.env.BASE_URL || "https://localhost:3500/api";
 
 const axiosInstace = axios.create({
   baseURL: BASE_URL,
