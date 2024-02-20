3503f94e6f6a06806d6421faeaeb36e0bbe9e773
Mazen-Abdalla <mazen.abdalla1@outlook.com>
2024-02-20 16:36:43 +0200

    api link update

--- app/src/api/axios.ts
+++ app/src/api/axios.ts
@@ -1,20 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "https://basic-auth-324k.onrender.com/api";
+const BASE_URL = "https://localhost:3500/api";
 
 const axiosInstace = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
 });
 
 const privateAxios = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
 });
 
 export { axiosInstace, privateAxios };
