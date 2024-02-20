7cb9bbb64dac52c775726de29abd702b465bcca4
Mazen-Abdalla <mazen.abdalla1@outlook.com>
2024-02-20 15:47:04 +0200

    deploy edit

--- app/src/api/axios.ts
+++ app/src/api/axios.ts
@@ -2,11 +2,19 @@ import axios from "axios";
 
 const BASE_URL = "https://basic-auth-324k.onrender.com/api";
 
-const axiosInstace = axios.create({ baseURL: BASE_URL });
+const axiosInstace = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+});
 
 const privateAxios = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export { axiosInstace, privateAxios };
