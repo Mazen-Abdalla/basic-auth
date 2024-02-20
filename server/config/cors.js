88ffc1adf88eb8846c1ef2661b7e8a5fc9ea9699
Mazen-Abdalla <mazen.abdalla1@outlook.com>
2024-02-20 17:48:15 +0200

    cors

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  "http://localhost:5173/",
-  "https://basic-auth-gamma.vercel.app/",
+  "http://localhost:5173",
+  "https://basic-auth-gamma.vercel.app",
   "*",
   null,
 ];
